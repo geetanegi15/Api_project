2c4211864c5626152f4349dac315adf8164efde8
geetanegi <geeta.negi@planetspark.in>
2023-08-08 12:15:15 +0530

    imp change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,8 +59,8 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  user_name:            'geeta.n@pixoatic.com',
-  password:             'zlvnhzazpzsrnvds',
+  user_name:            '************',
+  password:             '************',
   authentication:       'plain',
   enable_starttls_auto: true,
   open_timeout:         5,
